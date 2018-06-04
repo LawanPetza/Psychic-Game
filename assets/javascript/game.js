9e446d84f7132e1db3368be4e0082453f6a45efc
Lawan Petza <Lawan.Petza@gmail.com>
2018-06-04 02:43:33 -0400

    modified index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,16 +4,26 @@ var computerChoices = ["a", "b", "c","d","e", "f", "g", "h", "i","j", "k", "l", 
     var wins = 0;
     var losses = 0;
     var alphabet = 0;
-    var guessLeft = 9;
-    var guesses = [];
+    var guesses = 9;
+    var guessesLeft = 9;
+    var guessedLetters = [];
 
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     console.log(computerGuess);
+
+    var options = ["a", "b", "c","d","e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y","z"];
+
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
       // Determines which key was pressed.
-      var userGuess = event.key;
+      var userGuess = event.key.toLowerCase ();
+
+      var reset = function(){
+        totalGuesses = 9;
+        guessesLeft = 9;
+        guessedLetters = [];
+      }
 
       // Randomly chooses a choice from the options array. This is the Computer's guess.
      
@@ -22,26 +32,41 @@ var computerChoices = ["a", "b", "c","d","e", "f", "g", "h", "i","j", "k", "l", 
 
       // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
     //   if ((userGuess === "l") || (userGuess === "p") || (userGuess === "s")) {
-        guesses.push (userGuess); 
-        console.log(guesses)
+        // guessedLetters.push (userGuess); 
+        // console.log(guessedLetters)
 
+        
+      // if (guessesLeft > 0) {
+      if (options.indexOf(userGuess) > -1) {
+        
         if (userGuess === computerGuess)  {
           wins++;
-          guessLeft--;
-        } else if (userGuess !== computerGuess)  {
-          losses++;
-          guessLeft--;
+          reset();
+
+        } 
+        
+        if (userGuess !== computerGuess)  {
+          guessesLeft--;
+          guessedLetters.push(userGuess);
+        }
+
+        if (guessesLeft === 0) {
+          guessesLeft = 9;
+          losses ++;
+          guessedLetters = [];
+          reset();
         }
 
         // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
         var html =
-          "<p>You chose: " + userGuess + "</p>" +
+          // "<p>You chose: " + userGuess + "</p>" +
           "<p>wins: " + wins + "</p>" +
           "<p>losses: " + losses + "</p>" +
-          "<p>guesses left:" + guessLeft + "</p>" +
-          "<p>Your Guesses so far:" + guesses + "</p>";
+          "<p>Guesses Left: " + guessesLeft + "</p>" +
+          "<p>Your Guesses so far: " + guessedLetters + "</p>";
 
         // Set the inner HTML contents of the #game div to our html string
         document.querySelector("#game").innerHTML = html;
-    //   }
+
+      }
     };
